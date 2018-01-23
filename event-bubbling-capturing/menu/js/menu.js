03eeec805e6f2125cb905377064b79bbc73e9f3d
mesher23 <mesher23@gmail.com>
2018-01-23 22:45:55 +0400

    commit

---
+++ event-bubbling-capturing/menu/js/menu.js
@@ -0,0 +1,38 @@
+'use strict';
+
+function toggleMenu(event) {
+  const current = event.currentTarget;
+
+  if (current.classList.contains('show')) {
+    current.classList.remove('show');
+    current.classList.add('hide');
+  } else {
+    current.classList.add('show');
+    current.classList.remove('hide');
+  }
+}
+
+function openLink(event) {
+  event.preventDefault();
+  console.log(this.textContent);
+  event.stopPropagation();
+}
+
+function init(node) {
+  node.addEventListener('click', toggleMenu);
+}
+
+function initLink(node) {
+  if (node.dataset.toggle) {
+    return;
+  }
+  node.addEventListener('click', openLink, true);
+}
+
+Array
+  .from(document.querySelectorAll('.dropdown'))
+  .forEach(init);
+
+Array
+  .from(document.querySelectorAll('a'))
+  .forEach(initLink);
